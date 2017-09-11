335002aedaf39c3a28e59a384943d0c73fd92e04
emailman <emailman@dtcc.edu>
2017-09-11 10:23:04 -0400

    Add project files.

---
+++ CIS209-Random Number Demo/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace CIS209_Random_Number_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Location = new System.Drawing.Point(13, 13);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(159, 77);
+            this.btnRandom.TabIndex = 0;
+            this.btnRandom.Text = "Select Random Number";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.btnRandom);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRandom;
+    }
+}
+
---
+++ CIS209-Random Number Demo/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CIS209_Random_Number_Demo
+{
+    public partial class Form1 : Form
+    {
+        Random rand;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            int flip = rand.Next(2);
+            if (flip == 0)
+                MessageBox.Show("Heads");
+            else
+                MessageBox.Show("Tails");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rand = new Random();
+        }
+    }
+}
